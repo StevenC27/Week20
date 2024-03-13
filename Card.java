af203bccec5f875d3dcb09310a85fde58a2c37d8
StevenC27 <stevencurran.sc@gmail.com>
2024-03-13 19:09:18 +0000

    Fixed constructor

--- Card.java
+++ Card.java
@@ -2,11 +2,15 @@ public class Card {
     private String name;
     private Category[] categories = new Category[5];
 
-    public Card(int speed, int weight, int era, int legs, int wings){
+    public Card(String name, int speed, int weight, int era, int legs, int wings){
+        this.name = name;
+
         categories[0] = new Category("Speed", speed);
         categories[1] = new Category("Weight", weight);
         categories[2] = new Category("Era", era);
         categories[3] = new Category("Legs", legs);
         categories[4] = new Category("Wings", wings);
     }
+
+
 }

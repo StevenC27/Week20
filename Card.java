1a0ee84f4eb75ee7d2fa650936618896b6395caf
StevenC27 <stevencurran.sc@gmail.com>
2024-03-13 15:19:16 +0000

    Added constructor method initialising card name and categories.

--- Card.java
+++ Card.java
@@ -1,2 +1,12 @@
 public class Card {
+    private String name;
+    private Category[] categories = new Category[5];
+
+    public Card(int speed, int weight, int era, int legs, int wings){
+        categories[0] = new Category("Speed", speed);
+        categories[1] = new Category("Weight", weight);
+        categories[2] = new Category("Era", era);
+        categories[3] = new Category("Legs", legs);
+        categories[4] = new Category("Wings", wings);
+    }
 }

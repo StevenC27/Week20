60661cdd2b87291a364849029585e2b675da933f
StevenC27 <stevencurran.sc@gmail.com>
2024-03-13 14:51:18 +0000

    Added Category constructor method.

--- Category.java
+++ Category.java
@@ -1,2 +1,9 @@
 public class Category {
+    private String name;
+    private int value;
+
+    public Category(String name, int value){
+        this.name = name;
+        this.value = value;
+    }
 }

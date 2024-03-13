2932c7c79df4cdfb20cb6cdf55ec1f8f10725975
StevenC27 <stevencurran.sc@gmail.com>
2024-03-13 20:23:34 +0000

    Added constructor method.

--- Player.java
+++ Player.java
@@ -1,2 +1,9 @@
 public class Player {
+    private String name;
+    private Card[] cards;
+
+    public Player(String name, int totalCards){
+        this.name = name;
+        this.cards = new Card[totalCards];
+    }
 }
